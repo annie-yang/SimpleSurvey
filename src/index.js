ca5e8e801a68bcf9da174a49190d5dc1c263205d
Annie Y <annie_y48@hotmail.com>
2017-02-01 18:29:42 -0800

    Removed modules

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const fs = require('fs')
-const route = require('koa-route')
-
-function* index() {
-  this.body = fs.readFileSync(path.resolve(path.join('build', 'index.html')), 'utf8')
-}
-
 app.use(route.get('*', index))
 ReactDOM.render(
   <App />,
