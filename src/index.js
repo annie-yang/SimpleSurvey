db0a52cf0d239deed9eaca2ad2e161dc3f5e60a3
Annie Y <annie_y48@hotmail.com>
2017-02-01 18:27:48 -0800

    edited heroku deployment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const fs = require('fs')
+const route = require('koa-route')
+
+function* index() {
+  this.body = fs.readFileSync(path.resolve(path.join('build', 'index.html')), 'utf8')
+}
+
+app.use(route.get('*', index))
 ReactDOM.render(
   <App />,
   document.getElementById('root')
